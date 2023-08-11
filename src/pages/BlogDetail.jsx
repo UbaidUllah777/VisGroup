c1f717147039a41a75144514ec6239bf096f6825
Ubaid Ullah <khanxx777@gmail.com>
2023-08-11 17:43:27 +0500

    blogs detail rendering change

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -1,5 +1,19 @@
+// Libraries
 import React from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+// Components
+import Sidebar from "../components/common/header/sidebar/Sidebar";
+import Header from "../components/common/header/Header";
+import Seo from "../components/common/seo/Seo";
+import FooterMenuList from "../components/common/footer/FooterMenuList";
+import CopyrightFooter from "../components/common/footer/CopyrightFooter";
+
+// css
+import classes from "../components/blogs/blogDetail.module.css";
+
+// Data
 import { blogContent } from "../components/blogs/Blog";
 
 const BlogDetail = () => {
@@ -12,12 +26,102 @@ const BlogDetail = () => {
     return <div>Blog not found</div>;
   }
 
+  // Sort the blog content array by date in descending order
+  const sortedBlogs = [...blogContent].sort(
+    (a, b) => new Date(b.date) - new Date(a.date)
+  );
+
+  // Select the latest three blogs
+  const latestThreeBlogs = sortedBlogs.slice(0, 5);
   return (
-    <div>
-      {/* Display blog details */}
-      <h1>{blog.title}</h1>
-      <p>{blog.blogDescription}</p>
-      {/* Add more elements to display other details */}
+    <div className="main-page-wrapper">
+      <Seo title={blog.title} />
+
+      <Sidebar />
+
+      <Header />
+
+      {/* Banner:start */}
+      <div className="inside-hero-three">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-10 col-12">
+              <h2 className="page-title font-recoleta">{blog.title}</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Banner : End */}
+
+      {/* About Us: start */}
+      <div className="vcamp-text-block-one mt-20 lg-mt-120">
+        <div className="container">
+          <div className="row align-items-center" style={{ marginTop: "20px" }}>
+            <div className="col-md-8 col-12">
+              <div
+                className={classes.BlogImageWrapper}
+                style={{ paddingTop: "10px" }}
+              >
+                <img src={blog.img} alt={blog.title} />
+              </div>
+              <div className="row mt-50">
+                <div className="col-12">
+                  <p className={classes.blogDate}>{blog.date}</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Latest Blogs On SIde: Start */}
+            <div className="col-md-4 col-12">
+              <h5 className={classes.LatestBlogsHeading}>Latest Blogs</h5>
+              {latestThreeBlogs.map((item) => (
+                <li className={classes.LatestBlogsList} key={item.id}>
+                  <Link to={`/blogs/${item.id}`} className="title">
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </div>
+            {/* Latest Blogs On SIde: End */}
+          </div>
+        </div>
+      </div>
+      {/* About Us : ENd */}
+
+      {/* Footer : start */}
+      <footer className="vcamp-footer-two pt-150 lg-pt-100">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-3 col-lg-2 mb-40 d-flex align-items-center justify-content-center">
+              <div className="logo">
+                <Link to="/">
+                  <img
+                    src={require("../assets/images/logo/visLogo.png")}
+                    alt="Vis Logo"
+                    style={{ maxWidth: "200px", height: "auto" }}
+                  />
+                </Link>
+                <p style={{ fontFamily: "Recoleta" }}>
+                  We offer digital services in network benchmarking, software
+                  development, and mobile network testing. Contact us for
+                  details.
+                </p>
+              </div>
+            </div>
+            {/* End .col */}
+            <FooterMenuList />
+          </div>
+        </div>
+        {/* End .container */}
+
+        <div className="container">
+          <div className="bottom-footer">
+            <CopyrightFooter />
+          </div>
+        </div>
+        {/* End .container */}
+      </footer>
+      {/* Footer : End */}
     </div>
   );
 };
